0d133c94225c0d58c5a9922eb003d000a2188e24
Kimberly Simmonds <ksimmonds92@gmail.com>
2017-10-19 15:11:02 +0000

    Exercise 1

---
+++ maze.js
@@ -0,0 +1,13 @@
+window.onload =function1; 
+
+
+function function1(){
+//Ex 1
+
+
+document.getElementById('boundary1').addEventListener("mouseover",function(){
+	document.getElementById('boundary1').className='boundary youlose';
+});
+//object.onmouseover = function(){};
+
+}

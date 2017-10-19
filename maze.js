c6875b6b0845a83e52e86e632e76d1d228aa7f7d
Kimberly Simmonds <ksimmonds92@gmail.com>
2017-10-19 16:03:50 +0000

    Exercise 2

--- maze.js
+++ maze.js
@@ -1,13 +1,21 @@
 window.onload =function1; 
 
 
-function function1(){
-//Ex 1
+	function function1(){
+	//Ex 1
 
 
-document.getElementById('boundary1').addEventListener("mouseover",function(){
-	document.getElementById('boundary1').className='boundary youlose';
-});
-//object.onmouseover = function(){};
+	document.getElementById('boundary1').addEventListener("mouseover",function(){
+		document.getElementById('boundary1').className='boundary youlose';
+	});
 
+	//Ex 2
+	var boun = document.querySelectorAll(".boundary");
+	for(i=0;i<=boun.length;i++){
+		boun[i].onmouseover=function(){
+			for(var j =0;j<boun.length;j++){
+				boun[j].className="boundary youlose";
+			}
+		}
+	}
 }
